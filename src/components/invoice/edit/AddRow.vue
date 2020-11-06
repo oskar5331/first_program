ea91415a6e54859605d3e7b879840cafa0135a8e
Oskar Oja <oskar.oja@gmail.com>
2020-11-06 23:11:00 +0200

    invoice list good

---
+++ src/components/invoice/edit/AddRow.vue
@@ -0,0 +1,56 @@
+<template>
+  <tr>
+    <td><input type="text" name="name"  v-model="name"></td>
+    <td><input type="text" name="unit"  v-model="unit"></td>
+    <td><input type="text" name="count"  v-model="count"></td>
+    <td><input type="text" name="price"  v-model="price"></td>
+    <td><input type="text" name="sum"  v-model="sum"></td>
+    <td><button @click="add()">Add</button></td>
+  </tr>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+export default {
+  emits: ["add-row"],
+  setup(_, context) {
+    const name = ref("muna");
+    const unit = ref("tk") ;
+    const count = ref("1");
+    const price = ref("0.99");
+    const sum = ref("0.99");
+
+    const invalidInput = ref(false);
+    
+    const newRow = computed(function() {
+      return [name.value, unit.value, count.value,
+        price.value, sum.value];
+    });
+
+    function add() {
+      console.log("checking");
+      if (name.value === "" || unit.value === "" || count.value === "" || 
+        price.value === "" || sum.value === "") {
+        invalidInput.value = false;
+        return;
+      }
+      console.log("sending" + newRow.value);
+      context.emit("add-row", newRow.value);
+
+    }
+    return {
+      name,
+      unit,
+      count,
+      price,
+      sum,
+      add
+    };
+
+  }
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Invoice.vue
+++ src/views/Invoice.vue
@@ -1,14 +1,48 @@
 <template>
-  <ServiceList/>
+  <h1>Invoice</h1>
+  <table>
+    <thead>
+        <td>Name</td>
+        <td>Unit</td>
+        <td>Count</td>
+        <td>Price</td>
+        <td>Sum</td>
+    </thead>
+    <tbody>
+      <ServiceList :serviceList="rows"/>
+      <AddRow @add-row="addRow"/>
+      
+    </tbody>
+  </table>
 </template>
 
 <script>
+import { ref, computed } from "vue";
 import ServiceList from "@/components/invoice/ServiceList.vue";
+import AddRow from "@/components/invoice/edit/AddRow.vue";
 export default {
   components: {
-    ServiceList
+    ServiceList,
+    AddRow
+  },
+  setup() {
+    const rows = ref([["sai","tk","2","1.50","3"], 
+      ["kohupiim","tk","4","0.99","3.95"]]); // name,unit,count,price,sum
+    
+    // const insertedRows = computed(function() {
+    //   return rows;
+    // });
+
+    function addRow(data) {
+      rows.value.push(data);
+    }
+    
+    return {
+      rows,
+      addRow
+    };
   }
-}
+};
 </script>
 
 <style>
